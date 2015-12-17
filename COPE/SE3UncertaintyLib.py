b79c992ff0588293e6e37d69586b484682fee078
dinhhuy2109 <huy.nguyendinh09@gmail.com>
2015-12-17 12:37:23 +0800

    added coverge condition for particle filter

--- COPE/SE3UncertaintyLib.py
+++ COPE/SE3UncertaintyLib.py
@@ -1,9 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015 Huy Nguyen <huy.nguyendinh09@gmail.com>
+#
+# This file is part of python-cope.
+#
+# python-cope is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# python-cope is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# python-cope. If not, see <http://www.gnu.org/licenses/>.
+
 import numpy as np
 import scipy.linalg
 # Plots
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
-
 
 def TransformInv(T):
   """
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='COPE', 
-	version='1.0',
+	version='1.0.0',
 	description='Covariance-based Poses Estimation library',
 	author='Huy Nguyen',
 	author_email='huy.nguyendinh09@gmail.com',
