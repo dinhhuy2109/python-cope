97a83a7aa26fbb1b099d6bf92b762dab5a59bbde
dinhhuy <huy.nguyendinh09@gmail.com>
2017-02-27 17:14:37 +0800

    add propagating uncertainty w separated rot, trans

--- COPE/SE3UncertaintyLib.py
+++ COPE/SE3UncertaintyLib.py
@@ -523,6 +523,26 @@ def Propagating(T1, sigma1, T2, sigma2, method = 2):
     sigma = sigma1 + sigma2prime + 1/12.*(np.dot(A1,sigma2prime)+np.dot(sigma2prime,A1.T) + np.dot(sigma1,A2) + np.dot(sigma1,A2.T)) + B/4.   
   return T, sigma
 
+def PropagatingWithSeparateRotTrans(R1,sigmaR1,t1,sigmat1,R2,sigmaR2,t2,sigmat2):
+    """
+    Find the total uncertainty in a compound spatial relation (Compounding two uncertain transformations) where we separate rotation and translation
+    @param sigmaR1,sigmaR2: cov of Rot vec (RotToVec(T[:3,:3]))
+    @param sigmat1,sigamt2: cov of Trans vec (T[:3,3])
+    """
+    # Compound the means
+    R = np.dot(R1,R2)
+    t = np.dot(R1,t2)+t1
+    # Compute the cov of the compounding rot
+    sigmaR2prime = np.dot(np.dot(R1,sigmaR2),np.transpose(R1))
+    A1 = CovOp1(sigmaR1)
+    A2 = CovOp1(sigmaR2prime)
+    B = CovOp2(sigmaR1, sigmaR2prime)
+    sigmaR = sigmaR1 + sigmaR2prime + 1/12.*(np.dot(A1,sigmaR2prime) + np.dot(sigmaR2prime,A1.T) + np.dot(sigmaR1,A2) + np.dot(sigmaR1,A2.T)) + B/4.
+    # Compute the cov of teh compounding trans
+    R1t2 = np.dot(R1,t2)
+    sigmat = sigmat1 + np.dot(np.dot(R1,sigmat2),np.transpose(R1)) + np.dot(np.dot(Hat(R1t2),sigmaR1),np.transpose(Hat(R1t2)))
+    return R, sigmaR, t, sigmat
+
 
 def Fusing(Tlist, sigmalist, N = 0, maxiterations=30, retiter=False):
   """
