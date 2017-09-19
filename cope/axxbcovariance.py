162519f82356a010b22df1313d974bedb0623534
dinhhuy2109 <huy.nguyendinh09@gmail.com>
2017-09-19 14:35:09 +0800

    visualize

--- cope/axxbcovariance.py
+++ cope/axxbcovariance.py
@@ -34,6 +34,134 @@ def Eigsorted(cov):
     order = vals.argsort()[::-1]
     return vals[order], vecs[:,order]
 
+def VisualizeCovariances(cov_rot, cov_trans, minx,maxx,miny,maxy):
+  plt.subplot(231) # x and y axis
+  nstd=1
+  alpha = 0.5
+  mean = (0,0)
+  cov0 = cov_rot
+
+  cov = [[cov0 [0][0],cov0 [0][1]],[cov0 [1][0],cov0 [1][1]]]
+  pos = mean
+
+  ax = plt.gca()
+  vals, vecs = Eigsorted(cov)
+  theta = np.degrees(np.arctan2(*vecs[:,0][::-1]))
+  # Width and height are "full" widths, not radius
+  width, height = 2 * nstd * np.sqrt(vals)
+  ellip1 = Ellipse(xy=pos, width=width, height=height, angle=theta,alpha=0.5,color='red', linewidth=2, fill=False)
+  ax.add_artist(ellip1)
+  plt.axis([minx,maxx,miny,maxy])
+  plt.xlabel(r'${\bf{\xi}}_{\bf{R} x} (rad)$',fontsize=20, labelpad=8)
+  plt.ylabel(r'${\bf{\xi}}_{\bf{R} y} (rad)$',fontsize=20, labelpad=-8)
+  plt.xticks(np.arange(minx, maxx+maxx/2, (maxx-minx)/2))
+  plt.yticks(np.arange(miny, maxy+maxy/2, (maxy-miny)/2))
+  ax.set(aspect='equal')
+
+
+  plt.subplot(234)
+  mean = (0,0)
+  cov = cov_trans
+  cov = [[cov [0][0],cov [0][1]],[cov [1][0],cov [1][1]]]
+  pos=mean
+  ax = plt.gca()
+  vals, vecs = Eigsorted(cov)
+  theta = np.degrees(np.arctan2(*vecs[:,0][::-1]))
+  # Width and height are "full" widths, not radius
+  width, height = 2 * nstd * np.sqrt(vals)
+  ellip2 = Ellipse(xy=pos, width=width, height=height, angle=theta,alpha=0.5,color='red', linewidth=2, fill=False)
+  ax.add_artist(ellip2)
+  plt.axis([minx,maxx,miny,maxy])
+  plt.xlabel(r'${\bf{\xi}}_{\bf{t} x} (m)$',fontsize=20, labelpad=8)
+  plt.ylabel(r'${\bf{\xi}}_{\bf{t} y} (m)$',fontsize=20, labelpad=-8)
+  plt.xticks(np.arange(minx, maxx+maxx/2, (maxx-minx)/2))
+  plt.yticks(np.arange(miny, maxy+maxy/2, (maxy-miny)/2))
+  ax.set(aspect='equal')
+ ############################################################
+
+  plt.subplot(232)
+  cov = cov_rot
+  cov = [[cov0 [1][1],cov0 [1][2]],[cov0 [2][1],cov0 [2][2]]]
+  pos = mean
+
+  ax = plt.gca()
+  vals, vecs = Eigsorted(cov)
+  theta = np.degrees(np.arctan2(*vecs[:,0][::-1]))
+  # Width and height are "full" widths, not radius
+  width, height = 2 * nstd * np.sqrt(vals)
+  ellip1 = Ellipse(xy=pos, width=width, height=height, angle=theta,alpha=0.5,color='red', linewidth=2, fill=False)
+  ax.add_artist(ellip1)
+  plt.axis([minx,maxx,miny,maxy])
+  plt.xlabel(r'${\bf{\xi}}_{\bf{R} y} (rad)$',fontsize=20, labelpad=8)
+  plt.ylabel(r'${\bf{\xi}}_{\bf{R} z} (rad)$',fontsize=20, labelpad=-8)  
+  plt.xticks(np.arange(minx, maxx+maxx/2, (maxx-minx)/2))
+  plt.yticks(np.arange(miny, maxy+maxy/2, (maxy-miny)/2))
+  plt.legend(handles=[ellip1, ellip2],loc='upper center',ncol=2, bbox_to_anchor=(0.5,1.5))
+  ax.set(aspect='equal')
+
+  plt.subplot(235)
+  mean = (0,0)
+  cov = cov_trans
+  cov = [[cov [1][1],cov [1][2]],[cov [2][1],cov [2][2]]]
+  pos=mean
+  ax = plt.gca()
+  vals, vecs = Eigsorted(cov)
+  theta = np.degrees(np.arctan2(*vecs[:,0][::-1]))
+  # Width and height are "full" widths, not radius
+  width, height = 2 * nstd * np.sqrt(vals)
+  ellip2 = Ellipse(xy=pos, width=width, height=height, angle=theta,alpha=0.5,color='red', linewidth=2, fill=False)
+  ax.add_artist(ellip2)
+  plt.axis([minx,maxx,miny,maxy])
+  plt.xlabel(r'${\bf{\xi}}_{\bf{t} y} (m)$',fontsize=20, labelpad=8)
+  plt.ylabel(r'${\bf{\xi}}_{\bf{t} z} (m)$',fontsize=20, labelpad=-8)
+  plt.xticks(np.arange(minx, maxx+maxx/2, (maxx-minx)/2))
+  plt.yticks(np.arange(miny, maxy+maxy/2, (maxy-miny)/2))
+  ax.set(aspect='equal')
+
+  ###################################################################
+
+  plt.subplot(233)
+  cov = cov_rot
+  cov = [[cov0 [0][0],cov0 [0][2]],[cov0 [2][0],cov0 [2][2]]]
+  pos = mean
+
+  ax = plt.gca()
+  vals, vecs = Eigsorted(cov)
+  theta = np.degrees(np.arctan2(*vecs[:,0][::-1]))
+  # Width and height are "full" widths, not radius
+  width, height = 2 * nstd * np.sqrt(vals)
+  ellip1 = Ellipse(xy=pos, width=width, height=height, angle=theta,alpha=0.5,color='red', linewidth=2, fill=False)
+  ax.add_artist(ellip1)
+  plt.axis([minx,maxx,miny,maxy])
+  plt.xticks(np.arange(minx, maxx+maxx/2, (maxx-minx)/2))
+  plt.yticks(np.arange(miny, maxy+maxy/2, (maxy-miny)/2))
+  plt.xlabel(r'${\bf{\xi}}_{\bf{R} x} (rad)$',fontsize=20, labelpad=8)
+  plt.ylabel(r'${\bf{\xi}}_{\bf{R} z} (rad)$',fontsize=20, labelpad=-8)
+  ax.set(aspect='equal')
+  plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.8, hspace=None)
+
+  plt.subplot(236)
+  mean = (0,0)
+  cov = cov_trans
+  cov = [[cov [0][0],cov [0][2]],[cov [2][0],cov [2][2]]]
+
+  pos=mean
+  ax = plt.gca()
+  vals, vecs = Eigsorted(cov)
+  theta = np.degrees(np.arctan2(*vecs[:,0][::-1]))
+  # Width and height are "full" widths, not radius
+  width, height = 2 * nstd * np.sqrt(vals)
+  ellip2 = Ellipse(xy=pos, width=width, height=height, angle=theta,alpha=0.5,color='red', linewidth=2, fill=False)
+  ax.add_artist(ellip2)
+  plt.axis([minx,maxx,miny,maxy])
+  plt.xticks(np.arange(minx, maxx+maxx/2, (maxx-minx)/2))
+  plt.yticks(np.arange(miny, maxy+maxy/2, (maxy-miny)/2))
+  plt.xlabel(r'${\bf{\xi}}_{\bf{t} x}(m)$',fontsize=20, labelpad=8)
+  plt.ylabel(r'${\bf{\xi}}_{\bf{t} z}(m)$',fontsize=20, labelpad=-8)
+  ax.set(aspect='equal')
+  plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.8, hspace=None)
+  return True
+
 def VisualizeRealEstCov(cov_real, cov_est, minx,maxx,miny,maxy,param):
     if param=='rot':
         subplotnum = 230
